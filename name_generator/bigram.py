72f058a0ae704e756e4eef6782c1b30854c5affc
Moaz <156330236+Eng-Moaz@users.noreply.github.com>
2026-04-28 22:42:20 +0300

    Added a fully functional NN Bigram

--- name_generator/bigram.py
+++ name_generator/bigram.py
@@ -1,5 +1,6 @@
 import torch
 import os
+import torch.nn.functional as F
 
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(__file__))
@@ -43,3 +44,57 @@ class Bigram:
                     break
             print(''.join(out_names))
 
+
+class NNBigram:
+    def __init__(self):
+        self.x = []
+        self.y = []
+        self.w = None
+        self.stoi = {letter:i+1 for i, letter in enumerate(LETTERS)}
+        self.stoi["."] = 0
+        self.itos = {value:key for key, value in self.stoi.items()}
+        self._load_data()
+
+    def _load_data(self):
+        for name in NAMES:
+            chs = ["."] + list(name) + ["."]
+            for x, y in zip(chs, chs[1:]):
+                x_int, y_int = self.stoi[x], self.stoi[y]
+                self.x.append(x_int)
+                self.y.append(y_int)
+
+        self.x = torch.tensor(self.x)
+        self.y = torch.tensor(self.y)
+        self.x = F.one_hot(self.x, num_classes=27).float()
+        self.w = torch.randn((27, 27), requires_grad=True)
+
+    def train(self, epochs, lr):
+        for i in range(epochs):
+            self.w.grad = None
+            # Forward
+            logits = self.x @ self.w
+            probs = logits.exp()
+            softmax = probs / torch.sum(probs, dim=1, keepdim=True)
+            loss = -softmax[torch.arange(self.x.shape[0]),self.y].log().mean() + 0.01*(self.w**2).mean()
+
+            # Backward
+            loss.backward()
+            self.w.data -= lr * self.w.grad
+
+            #print(f"{loss.item():.4f}")
+
+    def generate(self, n):
+        g = torch.Generator()
+        for i in range(n):
+            out = []
+            i = 0
+            while True:
+                current = F.one_hot(torch.tensor([i]), num_classes=27).float()
+                logits = current @ self.w
+                probs = logits.exp()
+                softmax = probs / torch.sum(probs, dim=1, keepdim=True)
+                i = torch.multinomial(softmax, num_samples=1, replacement=True, generator=g).item()
+                out.append(self.itos[i])
+                if i == 0:
+                    break
+            print(''.join(out))
--- name_generator/cli.py
+++ name_generator/cli.py
@@ -1,5 +1,5 @@
 import argparse
-from bigram import Bigram
+from bigram import Bigram, NNBigram
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="name generator")
@@ -8,6 +8,11 @@ def main() -> None:
     bigram = subparsers.add_parser("bigram", help="Uses the bigram model to generate names")
     bigram.add_argument("-n", type=int, help="number of generated names")
 
+    nnbigram = subparsers.add_parser("nnbigram", help="Uses the bigram model to generate names")
+    nnbigram.add_argument("--epochs", type=int, default=100, help="Number epochs for training")
+    nnbigram.add_argument("--lr", type=float, default=50, help="learning rate for training")
+    nnbigram.add_argument("-n", type=int, help="number of generated names")
+
     args = parser.parse_args()
 
     match args.command:
@@ -15,6 +20,11 @@ def main() -> None:
             bigram = Bigram()
             bigram.load_bigram() ; bigram.normalize()
             bigram.generate(args.n)
+
+        case "nnbigram":
+            model = NNBigram()
+            model.train(args.epochs, args.lr)
+            model.generate(args.n)
 
 if __name__ == "__main__":
     main()
